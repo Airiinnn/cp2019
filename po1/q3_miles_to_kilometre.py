c9b2d368e24061816acb4ef5e46ae4f1f9535f28
Airiinnn <47800337+Airiinnn@users.noreply.github.com>
2019-02-24 22:23:04 +0800

    Add files via upload

---
+++ po1/q3_miles_to_kilometre.py
@@ -0,0 +1,4 @@
+mile = float(input("Enter distance in miles: "))
+km = mile * 1.60934
+
+print("{0:.2f}".format(km))
---
+++ po1/q6_find_ascii_char.py
@@ -0,0 +1,3 @@
+code = int(input("Enter an integer between 0 and 127: "))
+
+print(chr(code))
