e2afd8ff015871b23e23ea02cf2eb87d2649e3f2
Airiinnn <47800337+Airiinnn@users.noreply.github.com>
2019-02-24 22:29:55 +0800

    Delete q2_calc_cylinder_volume.py

--- po1/q2_calc_cylinder_volume.py
+++
@@ -1,9 +0,0 @@
-import math
-
-radius = float(input("Enter radius of base: "))
-length = float(input("Enter length of cylinder: "))
-
-area = radius ** 2 * math.pi
-volume = area * length
-
-print(volume)
