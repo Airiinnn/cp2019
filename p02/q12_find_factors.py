e659bc9c9500f3f2e8ed08c411d0617505bafb39
Airiinnn <47800337+Airiinnn@users.noreply.github.com>
2019-03-02 22:57:40 +0800

    Create q12_find_factors.py

---
+++ p02/q12_find_factors.py
@@ -0,0 +1,16 @@
+ognum = int(input())
+num
+list = []
+i = 2
+
+while i ** 2 < ognum:
+  if i == 2 and num % i == 0:
+    num = num / i
+    list.append(i)
+  elif i % 2 != 0 and num % i == 0:
+    num = num / i
+    list.append(i)
+  else:
+    i += 1
+    
+print(list)
